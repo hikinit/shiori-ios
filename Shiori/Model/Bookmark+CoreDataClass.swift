bc879afc6511eec6f4c87a3f213f1018d62ab52b
hikinit <hikinit@pm.me>
2020-08-21 14:55:33 +0700

    Update Core Data Model and restructuring

---
+++ Shiori/Model/Bookmark+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bookmark+CoreDataClass.swift
+//  Shiori
+//
+//  Created by hikinit on 21/08/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bookmark)
+public class Bookmark: NSManagedObject {
+
+}
--- Shiori/Model/Series+CoreDataClass.swift
+++ Shiori/Model/Series+CoreDataClass.swift
@@ -11,5 +11,30 @@ import CoreData
 
 @objc(Series)
 public class Series: NSManagedObject {
+  func setStatus(_ status: Status) {
+    self.status = status.rawValue
+  }
 
+  func setKind(_ kind: Kind) {
+    self.kind = kind.rawValue
+  }
+
+  func setId(_ id: UUID = UUID()) {
+    self.id = id
+  }
+}
+
+extension Series {
+  enum Kind: String {
+    case webnovel = "Web Novel"
+    case lightnovel = "Light Novel"
+    case comic = "Comic"
+  }
+
+  enum Status: String {
+    case reading,
+         finished,
+         onhold,
+         dropped
+  }
 }

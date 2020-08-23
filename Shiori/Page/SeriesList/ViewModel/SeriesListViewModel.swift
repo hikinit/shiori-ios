23154f6dd27c47773cd416e3ace07a3b26c29db5
hikinit <hikinit@pm.me>
2020-08-23 09:16:02 +0700

    Add blank SeriesListViewModel

---
+++ Shiori/Page/SeriesList/ViewModel/SeriesListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SeriesListViewModel.swift
+//  Shiori
+//
+//  Created by hikinit on 22/08/20.
+//
+
+import Foundation
+
+class SeriesListViewModel {}
---
+++ ShioriUnitTests/SeriesListViewModelTests.swift
@@ -0,0 +1,10 @@
+//
+//  SeriesListViewModelTests.swift
+//  ShioriUnitTests
+//
+//  Created by hikinit on 22/08/20.
+//
+
+import XCTest
+
+class SeriesListViewModelTests: XCTestCase {}

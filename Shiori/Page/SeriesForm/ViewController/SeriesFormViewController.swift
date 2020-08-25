15bdab7cecd0ef0d84ce533761df126924cbe1e9
hikinit <hikinit@pm.me>
2020-08-25 14:25:53 +0700

    Add automatic update viewmodel property on SeriesFormViewController

--- Shiori/Page/SeriesForm/ViewController/SeriesFormViewController.swift
+++ Shiori/Page/SeriesForm/ViewController/SeriesFormViewController.swift
@@ -13,8 +13,9 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
   @IBOutlet weak var kindField: UITextField!
   @IBOutlet weak var statusField: UITextField!
   @IBOutlet weak var websiteField: UITextField!
+  @IBOutlet weak var saveButton: UIBarButtonItem!
 
-  @IBAction func textFieldDidEditingChange(_ sender: UITextField) {
+  @IBAction func titleFieldDidEditingChange(_ sender: UITextField) {
     updateViewModelProperties(textField: sender)
   }
   
@@ -25,16 +26,13 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
   @IBAction func saveDidTap(_ sender: UIBarButtonItem) {}
 
   var series: Series!
-  var viewModel: SeriesFormViewModel! {
-    didSet {
-      setupUI()
-    }
-  }
+  var viewModel: SeriesFormViewModel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     setupViewModel()
+    setupView()
     viewModel.viewDidLoad()
   }
 
@@ -46,7 +44,7 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
     )
   }
 
-  private func setupUI() {
+  private func setupView() {
     titleField.text = viewModel.title
     kindField.text = viewModel.kind
     statusField.text = viewModel.status
@@ -54,25 +52,31 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
 
     createPickerView(textField: kindField, item: viewModel.kindOptions)
     createPickerView(textField: statusField, item: viewModel.statusOptions)
+    saveButtonToggleEnable()
   }
 
   private func updateViewModelProperties(textField: UITextField) {
     switch textField.tag {
-    case 101:
-      viewModel.title = textField.text
-    case 102:
-      viewModel.kind = textField.text
-    case 103:
-      viewModel.status = textField.text
-    case 104:
-      viewModel.website = textField.text
-    default:
-      break
+      case 101: viewModel.title = textField.text
+      case 102: viewModel.kind = textField.text
+      case 103: viewModel.status = textField.text
+      case 104: viewModel.website = textField.text
+      default: break
     }
+
+    saveButtonToggleEnable()
+  }
+
+  private func saveButtonToggleEnable() {
+    saveButton.isEnabled = viewModel.saveButtonIsEnabled
   }
 
   private func createPickerView(textField: UITextField, item: [String]) {
-    let didSelect = { textField.text = $0 }
+    let didSelect: (String) -> () = { [weak self] in
+      textField.text = $0
+      self?.updateViewModelProperties(textField: textField)
+    }
+
     let dataSource = PickerDataSource(item: item, didSelect: didSelect)
 
     pickerDataSources.append(dataSource)

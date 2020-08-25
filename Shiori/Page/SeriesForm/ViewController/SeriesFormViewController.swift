f4da5c5e88feb2cfc4571f900842ec5fb5302685
hikinit <hikinit@pm.me>
2020-08-25 13:41:17 +0700

    Add update view model property on editing change

--- Shiori/Page/SeriesForm/ViewController/SeriesFormViewController.swift
+++ Shiori/Page/SeriesForm/ViewController/SeriesFormViewController.swift
@@ -14,6 +14,10 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
   @IBOutlet weak var statusField: UITextField!
   @IBOutlet weak var websiteField: UITextField!
 
+  @IBAction func textFieldDidEditingChange(_ sender: UITextField) {
+    updateViewModelProperties(textField: sender)
+  }
+  
   @IBAction func cancelDidTap(_ sender: UIBarButtonItem) {
     dismiss(animated: true)
   }
@@ -52,10 +56,25 @@ class SeriesFormViewController: UITableViewController, ViewControllerWithStorybo
     createPickerView(textField: statusField, item: viewModel.statusOptions)
   }
 
+  private func updateViewModelProperties(textField: UITextField) {
+    switch textField.tag {
+    case 101:
+      viewModel.title = textField.text
+    case 102:
+      viewModel.kind = textField.text
+    case 103:
+      viewModel.status = textField.text
+    case 104:
+      viewModel.website = textField.text
+    default:
+      break
+    }
+  }
+
   private func createPickerView(textField: UITextField, item: [String]) {
     let didSelect = { textField.text = $0 }
     let dataSource = PickerDataSource(item: item, didSelect: didSelect)
-    
+
     pickerDataSources.append(dataSource)
     textField.createPickerView(dataSource: dataSource)
   }
